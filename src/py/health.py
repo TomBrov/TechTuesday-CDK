671464569492e3dcbbfefba627933dfdfd5da711
tombrovender <tom.brovender@cellebrite.com>
2023-07-31 13:14:22 +0300

    health

---
+++ src/py/health.py
@@ -0,0 +1,10 @@
+import boto3
+import json
+import os
+from botocore.exceptions import ClientError
+
+# health check
+def handler(event: Event, context):
+    return {
+        "Message": "Hello from Lambda!
+    }
